599c3542e1f3360308daf5e28528635139df5e0d
oracle <oracle>
2023-07-31 23:00:32 -0700

    gradle files

---
+++ TMessagesProj/.cxx/Debug/3p193g38/x86/CMakeFiles/3.10.2/CMakeASM_NASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_NASM_COMPILER "/home/cun/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/yasm")
+set(CMAKE_ASM_NASM_COMPILER_ARG1 "-DELF")
+set(CMAKE_AR "/home/cun/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ar")
+set(CMAKE_ASM_NASM_COMPILER_AR "")
+set(CMAKE_RANLIB "/home/cun/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ranlib")
+set(CMAKE_ASM_NASM_COMPILER_RANLIB "")
+set(CMAKE_LINKER "/home/cun/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ld")
+set(CMAKE_ASM_NASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_NASM_COMPILER_ID "YASM")
+set(CMAKE_ASM_NASM_COMPILER_VERSION "")
+set(CMAKE_ASM_NASM_COMPILER_ENV_VAR "ASM_NASM")
+
+
+set(CMAKE_ASM_NASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_NASM_LINKER_PREFERENCE 0)
+
+
